3c70d6263f887506229f6946826aa15c99287d59
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2023-01-21 12:24:05 -0500

    first commit

--- loadbalancer/test-lb.sh
+++ loadbalancer/test-lb.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 LB_IP=$(kubectl get svc/foo-service -n lb -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
+echo "-------------------------------------------"
 echo "LoadBalancer URL http://${LB_IP}:5678"
+echo "-------------------------------------------"
 # should output foo and bar on separate lines 
-for _ in {1..10}; do
-  curl ${LB_IP}:5678
+for i in {1..10}
+do
+  curl ${LB_IP}:5678 -q
 done
