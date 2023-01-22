943af41f752ec63e8553c3790b20c16ff958c6ea
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2023-01-22 11:44:58 -0500

    working ingress

--- restart-dev.sh
+++ restart-dev.sh
@@ -4,23 +4,27 @@ kind delete cluster -n dev
 kind create cluster --config dev-cluster.yaml
 
 
-kubectl apply -f ./ingress/contour.yaml
-sleep 20s
-kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+# kubectl apply -f ./ingress/contour.yaml
+# sleep 60s
+# kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+# sleep 60s
 
 
 kubectl apply -f ./ingress/deploy-ingress-nginx.yaml
 kubectl apply -f ./loadbalancer/metallb-native.yaml
 sleep 150s
 
-kubectl apply -f echo-service.yaml
+kubectl create namespace echo
+kubectl apply -f ./ingress/echo-service.yaml
 
 docker network inspect -f '{{.IPAM.Config}}' kind
 kubectl apply -f ./loadbalancer/metallb-config-ipaddresspool.yaml
+sleep 30s
 
+kubectl create namespace lb
 kubectl apply -f ./loadbalancer/lb-demo-deploy.yaml
 
-./test-lb.sh
+./loadbalancer/test-lb.sh
 
 helm install prom-operator-01 prometheus-community/kube-prometheus-stack
 sleep 20s
